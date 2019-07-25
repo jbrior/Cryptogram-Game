7cd6c9e115d4a49938afaf649b533f89318576a4
Jesse Brior <briorj@gmail.com>
2019-07-24 20:38:24 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+# -------- MODULE IMPORTS --------
 import random
+# -------- CLASS IMPORTS --------
 from phrases import Phrases
+
+# -------- GLOBAL VARIABLES --------
 
 p_p = Phrases.phrases
 ref = Phrases.phrases[1]
@@ -9,10 +13,21 @@ p_unique_letters = []
 p_unique_nums = []
 converted_phrase = ""
 
+""" 
+    THIS FUNCTION GENERATES A RANDOM NUMBER BETWEEN 1 & 26
+    (NUMBER OF LETTERS IN THE ALPHABET)
+"""
 def get_rand_num():
     r = random.randint(1, 26)
     return r
 
+
+"""
+    THIS FUNCTION TAKES THE ORIGINAL PHRASE AND
+    ITERATES OVER EVERY LETTER AND CHANGES IT TO
+    A RANDOM LETTER BASED ON THE RANDOM NUMBER 
+    THAT IS ASSIGNED IN THE 'RUN_PUZZLE' FUNCTION
+"""
 def create_new_p():
     global converted_phrase
     for l in p_p[phrase_picked]:
@@ -20,6 +35,11 @@ def create_new_p():
             temp_li = p_unique_letters.index(l.lower())
             temp_ni = p_unique_nums[temp_li] - 1
             converted_phrase = converted_phrase + p_a[temp_ni]
+
+        # IF THERE IS A SPECIAL CHARACTER IN THE PHRASE LIKE A PERIOD
+        # OR COLON. THESE STATEMENTS WILL NOT CONVERT THE CHARACTER TO
+        # A RANDOM CHARACTER
+
         elif l == '\'':
             converted_phrase = converted_phrase + "'"
         elif l == '\"':
@@ -35,9 +55,20 @@ def create_new_p():
         elif l == '-':
             converted_phrase = converted_phrase + '-'
 
+    """ 
+        PRINTS OUT THE FINAL CONVERTED PHRASE TO THE CONSOLE
+        ALONG WITH THE VERSES REFERENCE WHICH IS IN IT'S ORIGINAL
+        SPELLING
+    """
     print("\n" + converted_phrase.upper() + "\n\n" + ref)
 
-def run_game():
+
+"""
+    THIS FUNCTION ITERATES OVER THE ORIGINAL PHRASE AND PUTS ONLY THE
+    **DIFFERENT** CHARACTERS INTO A LIST. EACH CHARACTER IS THEN ASSIGNED
+    A RANDOM NUMBER BETWEEN 1 & 26.
+"""
+def run_puzzle():
     for l in p_p[phrase_picked]:
         if l.lower() not in p_unique_letters and l.lower() in p_a:
             p_unique_letters.append(l.lower())
@@ -49,9 +80,12 @@ def run_game():
         if r not in p_unique_nums:
             p_unique_nums.append(r)
 
-    #print(str(p_unique_letters) + ":" + str(p_unique_nums))
+    """ UNCOMENT BELOW LINE TO SEE A MORE CLEAR OUTPUT OF HOW
+        THE LETTERS ARE ASSIGNED TO A RANDOM NUMBER
+    """
+    # print(str(p_unique_letters) + ":" + str(p_unique_nums))
 
     create_new_p()
 
-
-run_game()
+    
+run_puzzle()
--- phrases.py
+++ phrases.py
@@ -1,7 +1,11 @@
 class Phrases():
 
+    # THIS IS A LIST THAT HOLDS EACH LETTER OF THE ALPHABET INDIVIDUALLY
     alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
              'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+    # HOLDS THE CURRENT PHRASE(S) IN A LIST.
+    # THE FIRST ITEM([0]) IS THE PHRASE
+    # THE SECOND ITEM([1]) IS THE VERSE REFERENCE
     phrases = ["Good will come to those who are generous and lend freely, "
                "who conduct their affairs with justice.", "Psalm - 112:5"]
