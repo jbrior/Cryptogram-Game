c55abe7e9273530d40038760b788f21440f8a80a
Jesse Brior <briorj@gmail.com>
2019-07-24 20:10:36 -0400

    Add files via upload

--- phrases.py
+++ phrases.py
@@ -4,4 +4,4 @@ class Phrases():
              'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     phrases = ["Good will come to those who are generous and lend freely, "
-               "who conduct their affairs with justice."]
+               "who conduct their affairs with justice.", "Psalm - 112:5"]
